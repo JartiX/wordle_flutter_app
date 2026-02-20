0d31af560286829394463f1a07d387e50c8fb29e
JartiX <148871057+JartiX@users.noreply.github.com>
2026-02-20 21:44:45 +0800

    Внедрение слов разной длины в игровую логику
    - Переработка репозитория слов
    - Переработка класса отвечающего за слова, теперь в нем есть возможность выбора длины слов
    - Внедрение нового функционала в модель игры и игрового контроллера

--- lib/repositories/word_repository.dart
+++ lib/repositories/word_repository.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/services.dart';
-import '../constants/assets.dart';
+import '../utils/word_repository_utils.dart';
 
 class WordRepository {
-  late final List<String> legalWords;
-  late final List<String> legalGuesses;
-  late final Set<String> allLegalGuesses;
+  final int length;
+   List<String>? _legalWords;
 
-  WordRepository._();
+  WordRepository._({required this.length});
 
-  static final WordRepository _instance = WordRepository._();
+  static final Map<int, WordRepository> _instances = {};
 
-  factory WordRepository() => _instance;
+  factory WordRepository(int length) {
+    return _instances.putIfAbsent(
+      length,
+      () => WordRepository._(length: length),
+    );
+  }
+List<String> get legalWords {
+    if (_legalWords == null || _legalWords!.isEmpty) {
+      throw Exception("Попытка доступа к словарю $length до загрузки!");
+    }
+    return _legalWords!;
+  }
+Future<void> load() async {
+    if (_legalWords != null) return;
 
-  Future<void> load() async {
-    final String wordsData = await rootBundle.loadString(Assets.legalWords);
-    legalWords = _parseWordsFromText(wordsData);
-
-    final String guessesData = await rootBundle.loadString(Assets.legalGuesses);
-    legalGuesses = _parseWordsFromText(guessesData);
-
-    allLegalGuesses = {...legalWords, ...legalGuesses};
+    try {
+      final String wordsAsset = assetFor();
+      final String wordsData = await rootBundle.loadString(wordsAsset);
+      _legalWords = _parseWordsFromText(wordsData);
+    } catch (e) {
+      rethrow; 
+    }
   }
 
   List<String> _parseWordsFromText(String text) {
--- lib/utils/word_utils.dart
+++ lib/utils/word_utils.dart
@@ -3,35 +3,25 @@ import '../models/types.dart';
 import '../repositories/word_repository.dart';
 
 extension WordUtils on Word {
-   
   bool get isLegalGuess {
-    if (!WordRepository().allLegalGuesses.contains(toString())) {
-      return false;
-    }
-
-    return true;
+    return WordRepository(length).legalWords.contains(toString());
   }
 
   Word evaluateGuess(Word other) {
     for (var i = 0; i < length; i++) {
       if (other[i].char == this[i].char) {
-        this[i] = (char: this[i].char, type: HitType.hit);
-        other[i] = (char: other[i].char, type: HitType.removed);
+        this[i].type = HitType.hit;
+        other[i].type = HitType.removed;
       }
     }
 
     for (var i = 0; i < other.length; i++) {
-
-      Letter targetLetter = other[i];
-      if (targetLetter.type != HitType.none) continue;
-
+      if (other[i].type != HitType.none) continue;
       for (var j = 0; j < length; j++) {
-        Letter guessedLetter = this[j];
-        if (guessedLetter.type != HitType.none) continue;
-
-        if (guessedLetter.char == targetLetter.char) {
-          this[j] = (char: guessedLetter.char, type: HitType.partial);
-          other[i] = (char: targetLetter.char, type: HitType.removed);
+        if (this[j].type != HitType.none) continue;
+        if (this[j].char == other[i].char) {
+          this[j].type = HitType.partial;
+          other[i].type = HitType.removed;
           break;
         }
       }
@@ -39,7 +29,7 @@ extension WordUtils on Word {
 
     for (var i = 0; i < length; i++) {
       if (this[i].type == HitType.none) {
-        this[i] = (char: this[i].char, type: HitType.miss);
+        this[i].type = HitType.miss;
       }
     }
 
