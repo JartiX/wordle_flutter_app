391cc99e1089ee35e02b7454ea7bcd08b5b24888
JartiX <148871057+JartiX@users.noreply.github.com>
2026-02-21 13:24:14 +0800

    Добавление клавиатуры в разметку
    Убрано поле ввода, вместо этого прямо в поле игры можно вводить буквы
    Добавлена клавиатура, на которой подсвечиваются использованные буквы
      - зеленый: попал
      - фиолетовый : не попал
      - янтарный: буква не на своем месте

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,14 +1,17 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'models/types.dart';
 import 'widgets/game_over_dialog.dart';
-import 'widgets/guess_input.dart';
+import 'widgets/buttons/animated_send_button.dart';
 import 'widgets/tile.dart';
 import 'widgets/top_toast.dart';
 import 'controllers/audio_controller.dart';
 import 'controllers/game_controller.dart';
 import 'models/word.dart';
+import 'constants/keyboard_layouts.dart';
+import 'widgets/game_keyboard.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({
@@ -47,36 +50,72 @@ class _GamePageState extends State<GamePage> {
     List<Word> guesses,
     double tileSize,
   ) {
-    if (rowIndex < widget.gameController.activeIndex) {
-      final char = guesses[rowIndex][colIndex].char;
-      final type = guesses[rowIndex][colIndex].type;
+    final submittedCount = widget.gameController.submittedCount;
+    final bool isSubmittedRow = rowIndex < submittedCount;
+    final bool isActiveRow =
+        rowIndex == submittedCount &&
+        !widget.gameController.didWin &&
+        !widget.gameController.didLose;
 
-      return FutureBuilder(
-        key: ValueKey('row_${rowIndex}_col_$colIndex'),
-        future: Future.delayed(
-          Duration(milliseconds: 120 * colIndex),
-          () => type,
-        ),
-        builder: (context, snapshot) {
+    if (isSubmittedRow) {
+      final letter = guesses[rowIndex][colIndex];
+      return Tile(
+        letter.char,
+        letter.type,
+        size: tileSize,
+        animationDelay: colIndex * 150,
+        key: ValueKey("tile-$rowIndex-$colIndex"),
+      );
+    }
+
+    if (isActiveRow) {
+      return ValueListenableBuilder<String>(
+        valueListenable: widget.gameController.currentGuessNotifier,
+        builder: (context, currentGuess, _) {
+          String char = "";
+          bool isHint = false;
+
+          if (colIndex < currentGuess.length) {
+            char = currentGuess[colIndex];
+          } else {
+            final hint = widget.gameController.hintLetters[colIndex];
+            if (hint != null) {
+              char = hint.char;
+              isHint = true;
+            }
+          }
+
           return Tile(
-            snapshot.hasData ? char : '',
-            snapshot.hasData ? snapshot.data! : HitType.none,
+            char,
+            HitType.none,
             size: tileSize,
+            needOpacity: isHint,
+            key: ValueKey("tile-$rowIndex-$colIndex"),
           );
         },
       );
     }
 
-    if (rowIndex == widget.gameController.activeIndex) {
-      final hint = widget.gameController.hintLetters[colIndex];
+    return Tile(
+      '',
+      HitType.none,
+      size: tileSize,
+      key: ValueKey("tile-$rowIndex-$colIndex"),
+    );
+  }
 
-      if (hint != null) {
-        return Tile(hint.char, HitType.none, size: tileSize, needOpacity: true,);
-      }
+  void _handleKeyEvent(String key) {
+    if (widget.gameController.didWin || widget.gameController.didLose) return;
+    widget.audioController.playPop();
+
+    if (key == KeyboardLayouts.enterKey) {
+      widget.audioController.playSend();
+      widget.gameController.submitCurrentGuess();
+    } else if (key == KeyboardLayouts.deleteKey) {
+      widget.gameController.removeLetter();
+    } else if (RegExp(r'^[а-яА-Яa-zA-Z]$').hasMatch(key)) {
+      widget.gameController.addLetter(key);
     }
-
-    // 3. ПУСТЫЕ КЛЕТКИ (будущие ходы)
-    return Tile('', HitType.none, size: tileSize);
   }
 
   void _showGameOverDialog(bool didWin) {
@@ -114,10 +153,14 @@ class _GamePageState extends State<GamePage> {
       _showTopError(event.error!);
     } else if (event.status == GameStatus.won) {
       widget.audioController.playWin();
-      _showGameOverDialog(true);
+      Future.delayed(const Duration(milliseconds: 800), () {
+        if (mounted) _showGameOverDialog(true);
+      });
     } else if (event.status == GameStatus.lost) {
       widget.audioController.playLose();
-      _showGameOverDialog(false);
+      Future.delayed(const Duration(milliseconds: 800), () {
+        if (mounted) _showGameOverDialog(false);
+      });
     }
 
     widget.gameController.gameEvent.value = null;
@@ -126,149 +169,154 @@ class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
     final media = MediaQuery.of(context);
-    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
-    final topInset = media.padding.top; // статусбар
-    final appBarHeight = kToolbarHeight;
-    final theme = Theme.of(context);
+    // final theme = Theme.of(context);
 
-    final maxHeight = media.size.height - topInset - appBarHeight - 16;
+    final topPadding = media.padding.top;
+    final bottomPadding = media.padding.bottom;
 
-    final isKeyboardOpen = bottomInset > 0;
-
-    return Stack(
-      children: [
-        AnimatedPadding(
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.easeOutCirc,
-          padding: EdgeInsets.only(
-            bottom: bottomInset,
-            top: topInset + 8,
-            left: 8,
-            right: 8,
-          ),
-          child: Align(
-            alignment: Alignment.center,
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxHeight: maxHeight),
-
-              child: Container(
-                padding: const EdgeInsets.only(
-                  left: 8,
-                  right: 8,
-                  top: 4,
-                  bottom: 4,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: theme.cardColor.withValues(alpha: 0.9),
-                  boxShadow: [
-                    BoxShadow(
-                      color: theme.colorScheme.onPrimary.withValues(alpha: .1),
-                      blurRadius: 20,
-                      offset: const Offset(0, 10),
-                    ),
-                  ],
-                ),
-                child: SingleChildScrollView(
-                  reverse: isKeyboardOpen,
-                  physics: const ClampingScrollPhysics(),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ValueListenableBuilder<List<Word>>(
+    return KeyboardListener(
+      focusNode: FocusNode()..requestFocus(),
+      onKeyEvent: (event) {
+        if (event is KeyDownEvent) {
+          if (event.logicalKey == LogicalKeyboardKey.enter) {
+            _handleKeyEvent(KeyboardLayouts.enterKey);
+          } else if (event.logicalKey == LogicalKeyboardKey.backspace) {
+            _handleKeyEvent(KeyboardLayouts.deleteKey);
+          } else if (event.character != null) {
+            _handleKeyEvent(event.character!);
+          }
+        }
+      },
+      child: Stack(
+        children: [
+          Scaffold(
+            body: Container(
+              height: double.infinity,
+              padding: EdgeInsets.only(
+                top: topPadding,
+                bottom: bottomPadding > 0 ? bottomPadding : 16,
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 12,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
+                      child: ValueListenableBuilder<List<Word>>(
                         valueListenable: widget.gameController.guessesNotifier,
                         builder: (context, guesses, _) {
-                          final screenWidth = media.size.width;
-                          final screenHeight = maxHeight;
-                          const spacing = 5.0;
-                          const horizontalPadding = 32.0;
-                          const verticalPadding = 16.0;
+                          return LayoutBuilder(
+                            builder: (context, constraints) {
+                              final lettersCount =
+                                  widget.gameController.wordLength;
+                              final rowsCount =
+                                  widget.gameController.numAllowedGuesses;
+                              const spacing = 5.0;
 
-                          final lettersCount = widget.gameController.wordLength;
-                          final rowsCount =
-                              widget.gameController.numAllowedGuesses;
+                              final tileWidth =
+                                  (constraints.maxWidth -
+                                      (spacing * (lettersCount - 1))) /
+                                  lettersCount;
+                              final tileHeight =
+                                  (constraints.maxHeight -
+                                      (spacing * (rowsCount - 1))) /
+                                  rowsCount;
+                              final tileSize = min(
+                                tileWidth,
+                                tileHeight,
+                              ).clamp(30.0, 70.0);
 
-                          final availableWidth =
-                              screenWidth - horizontalPadding;
-                          final tileWidth =
-                              (availableWidth - spacing * (lettersCount - 1)) /
-                              lettersCount;
-
-                          const guessInputApproxHeight = 120.0;
-
-                          final availableHeight =
-                              screenHeight -
-                              guessInputApproxHeight -
-                              verticalPadding;
-
-                          final tileHeight =
-                              (availableHeight - spacing * (rowsCount - 1)) /
-                              rowsCount;
-
-                          final tileSize = min(tileWidth, tileHeight);
-
-                          return Column(
-                            spacing: 5.0,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              for (
-                                int rowIndex = 0;
-                                rowIndex < rowsCount;
-                                rowIndex++
-                              )
-                                Row(
-                                  spacing: 5.0,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    for (
-                                      int colIndex = 0;
-                                      colIndex < lettersCount;
-                                      colIndex++
-                                    )
-                                      _buildCell(
-                                        rowIndex,
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: List.generate(rowsCount, (rowIndex) {
+                                  return Padding(
+                                    padding: EdgeInsets.only(
+                                      bottom: rowIndex == rowsCount - 1
+                                          ? 0
+                                          : spacing,
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: List.generate(lettersCount, (
                                         colIndex,
-                                        guesses,
-                                        tileSize,
-                                      ),
-                                  ],
-                                ),
-                            ],
+                                      ) {
+                                        return Padding(
+                                          padding: EdgeInsets.only(
+                                            right: colIndex == lettersCount - 1
+                                                ? 0
+                                                : spacing,
+                                          ),
+                                          child: _buildCell(
+                                            rowIndex,
+                                            colIndex,
+                                            guesses,
+                                            tileSize,
+                                          ),
+                                        );
+                                      }),
+                                    ),
+                                  );
+                                }),
+                              );
+                            },
                           );
                         },
                       ),
-                      GuessInput(
-                        audioController: widget.audioController,
-                        gameController: widget.gameController,
-                        onRestart: () {
-                          widget.gameController.resetGame();
-                        },
-                        onSubmitGuess: (guess) =>
-                            widget.gameController.submitGuess(guess),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+
+                  Expanded(
+                    flex: 7,
+                    child: ValueListenableBuilder<String>(
+                      valueListenable:
+                          widget.gameController.currentGuessNotifier,
+                      builder: (context, guess, _) {
+                        return ValueListenableBuilder<Map<String, HitType>>(
+                          valueListenable:
+                              widget.gameController.keyboardStatuses,
+                          builder: (context, statuses, _) {
+                            return GameKeyboard(
+                              statuses: statuses,
+                              language:
+                                  widget.gameController.settings.language.value,
+                              onKeyTap: _handleKeyEvent,
+                              enterButton: AnimatedSendButton(
+                                isEnabled: widget.gameController.isRightLength(
+                                  guess,
+                                ),
+                                onPressed: () =>
+                                    _handleKeyEvent(KeyboardLayouts.enterKey),
+                                audioController: widget.audioController,
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-        ),
-
-        if (_errorMessage != null)
-          AnimatedPositioned(
-            duration: const Duration(milliseconds: 400),
-            curve: Curves.easeOut,
-            top: _showError ? 100 : -80,
-            left: 20,
-            right: 20,
-            child: AnimatedOpacity(
+          if (_errorMessage != null)
+            AnimatedPositioned(
               duration: const Duration(milliseconds: 400),
-              opacity: _showError ? 1 : 0,
-              child: TopToast(message: _errorMessage!),
+              curve: Curves.easeOut,
+              top: _showError ? 100 : -80,
+              left: 20,
+              right: 20,
+              child: AnimatedOpacity(
+                duration: const Duration(milliseconds: 400),
+                opacity: _showError ? 1 : 0,
+                child: TopToast(message: _errorMessage!),
+              ),
             ),
-          ),
-      ],
+        ],
+      ),
     );
   }
 }
