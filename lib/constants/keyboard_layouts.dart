5577a1ae381537c45ab8aa41d473031950a7affb
JartiX <148871057+JartiX@users.noreply.github.com>
2026-02-21 23:17:17 +0800

    Изменение раскладки

--- lib/constants/keyboard_layouts.dart
+++ lib/constants/keyboard_layouts.dart
@@ -8,12 +8,12 @@ class KeyboardLayouts {
     GameLanguage.ru: [
       ['Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ'],
       ['Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э'],
-      [deleteKey, 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', enterKey],
+      ['Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', deleteKey, enterKey],
     ],
     GameLanguage.en: [
       ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
       ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-      [deleteKey, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', enterKey],
+      ['Z', 'X', 'C', 'V', 'B', 'N', 'M', deleteKey, enterKey],
     ],
   };
 }
